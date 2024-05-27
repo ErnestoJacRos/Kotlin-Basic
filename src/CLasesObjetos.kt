d5fbe37196df0a1593df9bd23a774850d5796ab0
Ernesto Jacros <mas.info.jacros@gmial.com>
2024-05-27 13:12:45 -0600

    modificadores de acceso y anular metdos, atributos y construtores

--- src/CLasesObjetos.kt
+++ src/CLasesObjetos.kt
@@ -1,3 +1,6 @@
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
 fun main() {
     /*Encapsulamiento, Abstracción, Polimorfismo, Herenco
 * propiedades -> variables que específican los atibutos de la clases
@@ -18,9 +21,20 @@ fun main() {
 
     smartDevice = SmartLightDevice(deviceName = "Google Light", deviceCategory = "Utility")
     smartDevice.turnOn()
+    var smartHome: SmartHome = SmartHome(
+        SmartTvDevice("RokuTv","Entertaimnet"),
+           SmartLightDevice("Xiaomi Light","Uyility") )
+    newTopic("Provando")
+    smartHome.smartTvDevice.printDeviceInfo()
+    smartHome.smartLightDevice.printDeviceInfo()
 }
 
-open class SmartDevice constructor(val name: String, val category: String) {
+open class SmartDevice public constructor(val name: String, val category: String) {
+    var deviceStatus = "online"
+        protected set/*(value) {
+            field = value
+        } lo hemos como no hizo ninguna validación solo se pone "set"*/
+
     constructor(name: String, category: String, statusCode: Int) : this(name, category) {
         deviceStatus = when (statusCode) {
             0 -> "offline"
@@ -29,7 +43,7 @@ open class SmartDevice constructor(val name: String, val category: String) {
         }
     }
 
-    var deviceStatus = "online"
+
     /*var speakerVolume: Int = 2
         set(value) {
             if (value in 0..100){
@@ -48,28 +62,41 @@ open class SmartDevice constructor(val name: String, val category: String) {
     open fun turnOff() {
         deviceStatus = "off"
     }
+
+    fun printDeviceInfo() {
+        println("Device name: $name, category: $category, type: $deviceType")
+    }
 }
 
 //Relación IS-A
 class SmartTvDevice constructor(/*val nara: String = "Sara",*/ deviceName: String, deviceCategory: String) :
     SmartDevice(name = deviceName, category = deviceCategory) {
-    var speakerVolume = 2
+
+    override val deviceType = "Smart TV"
+    /*private var speakerVolume = 2
         set(value) {
             if (value in 0..100) {
                 field = value
             }
-        }
+        }*/
+    private var speakerVolume by RangeRegulator(2,0,100)
 
-    var channelNumber = 1
+    /*private var channelNumber = 1
         set(value) {
             if (value in 1..200) {
                 field = value
             }
-        }
+        }*/
+    private var channelNumber by RangeRegulator(1, 0, 200)
 
     fun increaseSpeakerVolume() {
         speakerVolume++
         println("Speaker volume increased to $speakerVolume")
+    }
+
+    fun decreaseVolume() {
+        speakerVolume--
+        println("Speaker volume decreased to $speakerVolume")
     }
 
     fun nextCahnnel() {
@@ -77,13 +104,21 @@ class SmartTvDevice constructor(/*val nara: String = "Sara",*/ deviceName: Strin
         println("Channel number increased to $channelNumber")
     }
 
+    fun previousChannel() {
+        channelNumber--
+        println("Channel number decreased to $channelNumber")
+    }
+
     override fun turnOn() {
         super.turnOn()
-        println("$name is turned on. Speaker volume is set to " +
-                " $speakerVolume and channel number is " + "set to $channelNumber.")
+        println(
+            "$name is turned on. Speaker volume is set to " +
+                    " $speakerVolume and channel number is " + "set to $channelNumber."
+        )
     }
+
     override fun turnOff() {
-         super.turnOff()
+        super.turnOff()
         println("$name turned off")
     }
 }
@@ -91,17 +126,24 @@ class SmartTvDevice constructor(/*val nara: String = "Sara",*/ deviceName: Strin
 //Relación IS-A
 class SmartLightDevice constructor(deviceName: String, deviceCategory: String) :
     SmartDevice(name = deviceName, category = deviceCategory) {
-    var brightnessLevel = 0
+    override val deviceType = "Smart Light"
+    /*private var brightnessLevel = 0
         set(value) {
             if (value in 0..100) {
                 field = value
             }
-        }
+        }*/
+    private var brightnessLevel by RangeRegulator(0, 0,100)
 
     fun increaseBrightness() {
         brightnessLevel++
         println("Brightness increased to $brightnessLevel")
     }
+    fun decreaseBrightness() {
+        brightnessLevel--
+        println("Brightness decreased to $brightnessLevel")
+    }
+
     override fun turnOn() {
         super.turnOn()
         brightnessLevel = 2
@@ -118,36 +160,110 @@ class SmartLightDevice constructor(deviceName: String, deviceCategory: String) :
 class SmartHome constructor(
     val smartTvDevice: SmartTvDevice,
     val smartLightDevice: SmartLightDevice
-    ) {
+) {
+    val encendidoTv = smartTvDevice.deviceStatus == "on"
+    val encendidoLight = smartLightDevice.deviceStatus == "on"
+
+    var deviceTurnOnCount = 0
+        private set
+
     fun turnOnTv() {
-        smartTvDevice.turnOn()
+        if (encendidoTv) {
+            deviceTurnOnCount++
+            smartTvDevice.turnOn()
+            deviceTurnOnCount++
+        }
     }
 
     fun turnOffTv() {
-        smartTvDevice.turnOff()
+        if (encendidoTv) {
+            deviceTurnOnCount--
+            smartTvDevice.turnOff()
+            deviceTurnOnCount--
+        }
     }
 
     fun increaseTvVolumen() {
-        smartTvDevice.increaseSpeakerVolume()
+        if (encendidoTv) {
+            smartTvDevice.increaseSpeakerVolume()
+        }
+    }
+
+    fun deacreaseTvVolume() {
+        if (encendidoTv){
+            smartTvDevice.decreaseVolume()
+        }
     }
 
     fun changeTvChannelToNext() {
-        smartTvDevice.nextCahnnel()
+        if (encendidoTv) {
+            smartTvDevice.nextCahnnel()
+        }
     }
+    fun changeTvCahnnelPrevious() {
+        if (encendidoTv){
+            smartTvDevice.previousChannel()
+        }
+    }
+
     fun turnOnLight() {
-        smartLightDevice.turnOn()
+        if (encendidoLight) {
+            deviceTurnOnCount++
+            smartLightDevice.turnOn()
+            deviceTurnOnCount++
+        }
     }
+
     fun turnOffLight() {
-        smartLightDevice.turnOff()
+        if (encendidoLight) {
+            deviceTurnOnCount--
+            smartLightDevice.turnOff()
+            deviceTurnOnCount--
+        }
     }
 
     fun increaseLightBrightness() {
-        smartLightDevice.increaseBrightness()
+        if (encendidoLight) {
+            smartLightDevice.increaseBrightness()
+        }
+    }
+    fun decreaseLighBrightness() {
+        if (encendidoLight){
+            smartLightDevice.decreaseBrightness()
+        }
     }
 
     fun turnOffAllDevices() {
-        turnOffTv()
-        turnOffLight()
+        if (encendidoTv && encendidoLight) {
+            turnOffTv()
+            turnOffLight()
+            deviceTurnOnCount = 0
+        }
+    }
+    fun printSmartLightInfo() {
+        smartTvDevice.printDeviceInfo()
+    }
+
+    fun printSmartTvInfo() {
+        smartLightDevice.printDeviceInfo()
+    }
+}
+
+class RangeRegulator constructor(
+    initialValue: Int,
+    private val minValue: Int,
+    private val maxValue: Int
+) : ReadWriteProperty<Any?, Int> {
+    var fieldData = initialValue
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        return fieldData
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+        if (value in minValue..maxValue){
+            fieldData = value
+        }
     }
 
 }
---
+++ src/kotlin14/ModificadoresDeVisibilidad.kt
@@ -0,0 +1,13 @@
+package kotlin14
+/*public -> modificador de acceso predeterminado
+* permite que las declaraciones sea accesible en todas partes
+* private -> solo permite se accesible en la misma clase
+* protected -> permite que la declaraciones sean accesicles
+* Internal -> permite que la delcaración sea accesible
+* en le mismo modulo similar a private puedes acceder a propiedas y
+* métodos internos desde fuera de la clase siempre y cuando lo
+* hagas en el mismo módulo*/
+fun main() {
+
+
+}
